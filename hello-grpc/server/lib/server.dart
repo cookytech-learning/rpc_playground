a405e0e0e21e21061574cdf1b8852e79a221b4d0
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-10-01 15:40:19 +0530

    adds the server and client projects and generated data from protoc

---
+++ hello-grpc/server/lib/server.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library server;
+
+export 'src/server_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
